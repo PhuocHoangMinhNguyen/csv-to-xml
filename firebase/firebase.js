4d67678d1851437fb5248d53a1653a682bd4c9eb
PhuocHoangMinhNguyen <phuochoangminh3699@gmail.com>
2026-03-12 13:11:44 +1100

    Update

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,6 +1,9 @@
 // Firebase
 const admin = require('firebase-admin');
-const serviceAccount = require('./csv-to-xml-ee901-firebase-adminsdk-gkkqc-1e6338e5e9.json');
+
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
+    ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+    : require('./csv-to-xml-ee901-firebase-adminsdk-gkkqc-1e6338e5e9.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
